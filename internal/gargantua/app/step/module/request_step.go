80b53d28fd0962d12fc23d5ebcf7797486c0b43a
dayeguilaiye <979014041@qq.com>
2022-04-27 23:54:47 +0800

    ADD: make requestStep executable

--- internal/gargantua/app/step/module/request_step.go
+++ internal/gargantua/app/step/module/request_step.go
@@ -2,6 +2,8 @@ package module
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/Galaxy-Railway/GGargantua/api/protobuf"
 	"github.com/Galaxy-Railway/GGargantua/internal/gargantua/domain/request/module"
 	request "github.com/Galaxy-Railway/GGargantua/internal/gargantua/domain/request/service"
@@ -24,6 +26,22 @@ func TransRequestStepTypeByPb(stepType *protobuf.RequestStepType) *RequestStepTy
 }
 
 func (f *RequestStepType) Execute(ctx context.Context, requestService request.RequestService, scriptService script.ScriptService) (*StepResult, error) {
-	//TODO implement me
-	panic("implement me")
+	stepResult := &StepResult{}
+	// todo: send ctx
+	result, err := requestService.SendSingleRequest(f.Request)
+	if err != nil {
+		stepResult.Success = false
+		stepResult.Reason = err.Error()
+	} else {
+		stepResult.Success = true
+		stepResult.Reason = "request send successful"
+		bytes, marshalErr := json.Marshal(result)
+		if marshalErr != nil {
+			stepResult.Success = false
+			stepResult.Reason = fmt.Sprintf("send request success, got response: %+v, \nbut failed to marshal into json, err: %+v", result, marshalErr)
+		} else {
+			stepResult.Result = string(bytes)
+		}
+	}
+	return stepResult, nil
 }
